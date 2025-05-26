7abdd1a0c44f057925a2e05aaeb0c1617cc8bdd5
Vojtech Nerad A895691 <vojtech.nerad@eviden.com>
2025-05-26 10:59:18 +0200

    todos as objects

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 const form = document.getElementById('todo-form')! as HTMLFormElement;
 
-let todos: Array<string> = [];
+type Todo = {
+  id: string;
+  text: string;
+  isCompleted: boolean;
+}
+
+let todos: Array<Todo> = [];
 
 const handleFormSubmit = (e: Event) => {
   e.preventDefault()
+
   const formData = new FormData(form);
-  const todoValue = formData.get('todo-text') as string;
-  todos = [...todos, todoValue]
+  const todo: Todo = {
+    id: crypto.randomUUID(),
+    text: formData.get('todo-text') as string,
+    isCompleted: false,
+  }
+  todos.push(todo);
   renderTodos()
 }
 
@@ -19,9 +30,25 @@ const renderTodos = () => {
   todos.forEach((value, index) => {
     const todoItemElem = document.createElement('li')
     const todoSpanElem = document.createElement('span')
-    todoSpanElem.innerHTML = value
+    todoSpanElem.innerHTML = value.text
+
+    if (value.isCompleted) todoItemElem.classList.add('completed')
+
+    todoItemElem.addEventListener('click', () => {
+      todos = todos.map((savedTodo): Todo =>
+        savedTodo.id === value.id
+          ? { ...savedTodo, isCompleted: !savedTodo.isCompleted }
+          : savedTodo)
+      renderTodos();
+    })
+
+    // Delete Button
     const deleteBtn = document.createElement('button')
     deleteBtn.innerHTML = 'delete'
+    deleteBtn.addEventListener('click', () => {
+      todos = todos.filter((savedTodo) => savedTodo.id !== value.id)
+      renderTodos();
+    })
 
     todoItemElem.appendChild(todoSpanElem)
     todoItemElem.appendChild(deleteBtn)
