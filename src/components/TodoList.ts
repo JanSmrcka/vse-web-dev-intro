2e11c88b2e8ceadb3ef669e71bc1a474ede1a56c
adamhanus <kecupwik@gmail.com>
2025-05-26 11:18:25 +0200

    refactor todo management by creating a TodoList class; separate Todo type definition and streamline form submission handling

---
+++ src/components/TodoList.ts
@@ -0,0 +1,71 @@
+import {Todo} from "../types"
+
+class TodoList {
+    todos: Todo[] = []
+    todoListElement
+
+    constructor(elementId: string){
+        this.todoListElement = document.getElementById('todo-list') as HTMLUListElement
+    }
+    
+    addTodo(todoValue: string) {
+        const newTodo: Todo = {
+            id: crypto.randomUUID(),
+            title: todoValue,
+            completed: false
+          }
+    
+          this.todos.push(newTodo)
+          this.render()    
+    }
+
+    removeTodo(id: string) {
+        this.todos = this.todos.filter((todo) => todo.id !== id)
+        this.render()
+    }
+
+    toggle(id: string) {
+        this.todos = this.todos.map((todo) => {
+            if (todo.id === id) {
+                return {...todo, completed: !todo.completed}
+            }
+            return todo
+        })
+        console.log(this.todos)
+        this.render()
+    }
+    
+
+    render() {
+        this.todoListElement.innerHTML = ''
+
+        this.todos.forEach((item) => {
+            const todoItemElement = document.createElement('li')
+            const todoSpanElement = document.createElement('span')
+            todoSpanElement.innerHTML = item.title
+
+            if (item.completed) {
+                todoItemElement.classList.add("completed")
+            }
+
+            todoItemElement.addEventListener("click", () => {
+                this.toggle(item.id)
+            })
+
+            const deleteButton = document.createElement('button')
+            deleteButton.innerHTML = "Delete"
+
+            deleteButton.addEventListener("click", (e) => {
+                e.stopPropagation()
+                this.removeTodo(item.id)
+            })
+
+            todoItemElement.appendChild(todoSpanElement)
+            todoItemElement.appendChild(deleteButton)
+
+            this.todoListElement.appendChild(todoItemElement)
+        })
+    }
+}
+
+export const todoList = new TodoList("todo-list")
--- src/main.ts
+++ src/main.ts
@@ -1,72 +1,14 @@
+import { todoList } from "./components/TodoList"
+
 const formElement = document.getElementById('todo-form') as HTMLFormElement
-
-type Todo = {
-    id: string
-    title: string
-    completed: boolean
-}
-
-let todos: Todo[] = []
 
 function handleFormSubmit(e: Event) {
   e.preventDefault()
   const formData = new FormData(formElement)
   const todoValue = formData.get('todo-text') as string
 
-  const newTodo: Todo = {
-    id: crypto.randomUUID(),
-    title: todoValue,
-    completed: false
-  }
-
-  todos.push(newTodo)
-  renderTodos()
+  todoList.addTodo(todoValue)
   formElement.reset()
 }
 
 formElement?.addEventListener("submit", handleFormSubmit) // Otaznik - pokud je element NULL, tak to projde a nespadne
-
-function renderTodos() {
-  const todoListElement = document.getElementById('todo-list') as HTMLUListElement
-  todoListElement.innerHTML = ''
-
-  todos.forEach((item) => {
-    const todoItemElement = document.createElement('li')
-    const todoSpanElement = document.createElement('span')
-    todoSpanElement.innerHTML = item.title
-
-    if (item.completed) {
-        todoItemElement.classList.add("completed")
-    }
-
-    todoItemElement.addEventListener("click", () => {
-        todos = todos.map((todo) => {
-            if (todo.id === item.id) {
-                return {...todo, completed: !todo.completed} //spread operator - kopie objektu a zmenin v tomto pripade completed
-            }
-            return todo
-        })
-        console.log(todos)
-        renderTodos()
-    })
-
-
-    const deleteButton = document.createElement('button')
-    deleteButton.innerHTML = "Delete"
-
-    deleteButton.addEventListener("click", (e) => {
-        e.stopPropagation()
-        todos = todos.filter((todo ) => todo.id !== item.id )
-        renderTodos()
-    })
-
-
-
-
-
-    todoItemElement.appendChild(todoSpanElement)
-    todoItemElement.appendChild(deleteButton)
-
-    todoListElement?.appendChild(todoItemElement)
-  })
-}
