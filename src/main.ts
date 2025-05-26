a378763be5ff8da934d2df47b425dbdaf3697951
Trong Dat Luu <david19au@gmail.com>
2025-05-26 10:34:51 +0200

    crypt uuid + array changes to reflect uniqueness

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
 const formElement = document.getElementById('todo-form')! as HTMLFormElement;
 
-const todos: string[] = []
+type Todo = {
+    id: string
+    text: string
+    completed: boolean
+}
+
+const todos: Todo[] = []
 
 function handleFormSubmit(e: Event){
     e.preventDefault()
     const formData = new FormData(formElement)
     const todoValue = formData.get("todo-text") as string
-    todos.push(todoValue)
+
+    const newTodo: Todo = {
+        id: crypto.randomUUID(),
+        text: todoValue,
+        completed: false
+    }
+    todos.push(newTodo)
     renderTodos()
     console.log(todos)
 }
@@ -17,10 +29,10 @@ function renderTodos() {
     const todoListElement = document.getElementById('todo-list') as HTMLUListElement
     todoListElement.innerHTML = ''
 
-    todos.forEach((value, index) => {
+    todos.forEach((todo) => {
         const todoItemElement = document.createElement('li')
         const todoSpanElement = document.createElement("span")
-        todoSpanElement.innerHTML = value
+        todoSpanElement.innerHTML = todo.text
         const deleteButton = document.createElement("button")
         deleteButton.innerHTML = 'delet'
 
