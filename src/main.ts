75821294849c4a8b7ca45cb08c20b62fd70d683e
Lucie Čondlová <lcondlova@seznam.cz>
2025-05-26 12:29:55 +0200

    added todo-list

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,14 @@
+import { todoList } from './components/todo-list'
+
 const formElement = document.getElementById('todo-form')! as HTMLFormElement
 
-const todos: string[] = []
 
 function handleFormSubmit(e: Event){
     e.preventDefault()
     const formData = new FormData(formElement)
     const todoValue = formData.get('todo-text') as string
-    todos.push(todoValue)
-    renderTodos()
-    console.log(todos)
+    todoList.addTodo(todoValue)
+    formElement.reset()
 }
 
 formElement.addEventListener('submit', handleFormSubmit)
-
-function renderTodos() {
-    const todoListElement = document.getElementById('todo-list') as HTMLUListElement
-    todoListElement.innerHTML = ''
-
-    todos.forEach((value) => {
-        const todoItemElement = document.createElement('li')
-        const todoSpanElement = document.createElement('span')
-        todoSpanElement.innerHTML = value
-        const deleteButton = document.createElement('button')
-        deleteButton.innerHTML = 'delete'
-
-        todoItemElement.appendChild(todoSpanElement)
-        todoItemElement.appendChild(deleteButton)
-
-        todoListElement?.appendChild(todoItemElement)
-    })
-}
-
-
