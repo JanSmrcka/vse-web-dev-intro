18cf6cb9e5dcde94b23d0ba464830ad29af82c16
kuza <kuzn03@vse.cz>
2025-05-26 11:23:03 +0200

    vytvoreni tridy

--- src/main.ts
+++ src/main.ts
@@ -1,69 +1,20 @@
+import { todoList } from "./components/todo-list"
+
 const formElement = document.getElementById('todo-form')! as HTMLFormElement
-
-type Todo = {
-    id: string 
-    text: string
-    completed: boolean
-}
-
-
-let todos: Todo[] = []
 
 function handleFormSubmit(e: Event) {
     e.preventDefault()
     const formData = new FormData(formElement)
     const todoValue = formData.get("todo-text") as string
 
-    const newTodo:Todo = {
-        id: crypto.randomUUID(),
-        text: todoValue,
-        completed: false
-    }
+    todoList.addTodo(todoValue)
 
-    todos.push(newTodo)
-    renderTodos()
     formElement.reset()
 }
 
 formElement.addEventListener("submit", handleFormSubmit)
 
 
-function renderTodos() {
-    const todoListElement  = document.getElementById("todo-list") as HTMLUListElement
-    todoListElement.innerHTML = ''
 
-    todos.forEach((item)=>{
-        const todoItemElement = document.createElement("li")
-        const todoSpanElement = document.createElement("span")
-        todoSpanElement.innerHTML = item.text
-        
-        if (item.completed){
-            todoItemElement.classList.add("completed")
-        }
-        
-        todoItemElement.addEventListener("click",()=>{
-            todos = todos.map((todo)=>{
-                if (todo.id == item.id) {
-                    return {...todo, completed: !todo.completed}
-                }
-                return todo
-            })
-            renderTodos()
 
-        })
-
-        const deleteButton = document.createElement("button")
-        deleteButton.innerHTML = "delete"
-
-        deleteButton.addEventListener("click", ()=> {
-            todos = todos.filter((todo)=>todo.id !== item.id)
-            renderTodos()
-        })
-
-        todoItemElement.appendChild(todoSpanElement)
-        todoItemElement.appendChild(deleteButton)
-
-        todoListElement?.appendChild(todoItemElement)
-    })
-}
 
