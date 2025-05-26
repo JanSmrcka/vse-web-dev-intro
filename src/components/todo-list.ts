1f2e2cf0e409e2506095bf7637e8233b53627d6a
adamhanus <kecupwik@gmail.com>
2025-05-26 13:57:30 +0200

    implement error handling in async methods of TodoList class for loading, adding, removing, and toggling todos

--- src/components/todo-list.ts
+++ src/components/todo-list.ts
@@ -11,64 +11,78 @@ class TodoList {
     }
 
     async loadTodos() {
+        try { //try catch clause for api error handling
         const newTodos = await todoService.fetchTodos()
         this.todos = newTodos
         this.render()
+        } catch (error) {
+            console.log(error)
+        } 
     }
     
     async addTodo(todoValue: string) {
-        const newTodo = await todoService.createTodo(todoValue)
-        this.todos.push(newTodo)
-        this.render()    
+        try {
+            const newTodo = await todoService.createTodo(todoValue)
+            this.todos.push(newTodo)
+            this.render()    
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     async removeTodo(id: number) {
-        await todoService.deleteTodo(id)
-        this.todos = this.todos.filter((todo) => todo.id !== id)
-        this.render()
+        try {
+            await todoService.deleteTodo(id)
+            this.todos = this.todos.filter((todo) => todo.id !== id)
+            this.render()
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     async toggle(id: number) {
-        const todo = this.todos.find((todo) => id === todo.id)
-        const newTodo = await todoService.toggleTodo(id, !todo?.completed)
-
-        this. todos = this.todos.map((todo) => {
-            if (todo.id === id) {
-                return newTodo
-            }
-            return todo
-        })
-        this.render()
+        try {
+            const todo = this.todos.find((todo) => id === todo.id)
+            const newTodo = await todoService.toggleTodo(id, !todo?.completed)
+            this. todos = this.todos.map((todo) => {
+                if (todo.id === id) {
+                    return newTodo
+                }
+                return todo
+            })
+            this.render()
+        } catch (error) {
+            console.log(error)
+        }
     }
     
-
     render() {
         this.todoListElement.innerHTML = ''
-
         this.todos.forEach((item) => {
+            // Generate elements
             const todoItemElement = document.createElement('li')
             const todoSpanElement = document.createElement('span')
             todoSpanElement.innerHTML = item.text
 
+            // Complete todo
             if (item.completed) {
                 todoItemElement.classList.add("completed")
             }
-
             todoItemElement.addEventListener("click", () => {
                 this.toggle(item.id)
             })
 
+            // Delete todo
             const deleteButton = document.createElement('button')
             deleteButton.innerHTML = "Delete"
-
             deleteButton.addEventListener("click", (e) => {
                 e.stopPropagation()
                 this.removeTodo(item.id)
             })
 
+            // Append elements to genereated corpse
             todoItemElement.appendChild(todoSpanElement)
             todoItemElement.appendChild(deleteButton)
-
             this.todoListElement.appendChild(todoItemElement)
         })
     }
