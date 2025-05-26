63e5ee492f03493ce5fd59042910cb8c2eddb8c7
Adam <adam.schindler@seznam.cz>
2025-05-26 11:22:51 +0200

    upravení todo-lists.ts, lepší čitelnost, construktor

--- src/components/todo-list.ts
+++ src/components/todo-list.ts
@@ -4,6 +4,11 @@ class TodoList {
   todos: Todo[] = []
   todoListElement = document.getElementById('todo-list') as HTMLUListElement
 
+    constructor(elementId: string){
+        this.todoListElement = document.getElementById(elementId) as HTMLUListElement
+    }
+
+
 addTodo(todoValue: string) {
   const newTodo: Todo = {
     id: crypto.randomUUID(),
@@ -13,6 +18,23 @@ addTodo(todoValue: string) {
 
   this.todos.push(newTodo)
   this.render()
+}
+
+removeTodo(id: string){
+    this.todos = this.todos.filter((todo) => todo.id !== id)
+        this.render()
+}
+
+
+toggle(id: string){
+    this.todos = this.todos.map((todo) => {
+          if (todo.id === id) {
+            return { ...todo, completed: !todo.completed }
+          }
+          return todo
+        })
+        console.log(this.todos)
+        this.render()
 }
 
 
@@ -30,23 +52,15 @@ addTodo(todoValue: string) {
       }
 
       todoItemElement.addEventListener('click', () => {
-        this.todos = this.todos.map((todo) => {
-          if (todo.id === item.id) {
-            return { ...todo, completed: !todo.completed }
-          }
-          return todo
-        })
-        console.log(this.todos)
-        this.render()
+        this.toggle(item.id)
       })
 
       const deleteButton = document.createElement('button')
       deleteButton.innerHTML = 'delete'
 
       deleteButton.addEventListener('click', () => {
-        this.todos = this.todos.filter((todo) => todo.id !== item.id)
-        console.log(this.todos)
-        this.render()
+        this.removeTodo(item.id)
+
       })
 
       todoItemElement.appendChild(todoSpanElement)
@@ -57,4 +71,4 @@ addTodo(todoValue: string) {
 }
 
 
-export const todoList = new TodoList()
+export const todoList = new TodoList("todo-list")
