49b7d294fc68846458186e2d136bcac8debe2d00
Adam Mrózek <adam.mrozek@outlook.cz>
2025-05-26 12:33:00 +0200

    Changed comment in Main

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,18 @@ import 'reflect-metadata';
 import { TodoListRenderer } from './renderers/todoList.renderer.ts'
 import { container } from 'tsyringe';
 import { TodoRepository } from './repositories/todo.repository.ts'
+import { Todo } from './types/todo.type.ts'
 
 class Main {
   private readonly todoListRenderer: TodoListRenderer;
+  private readonly todoRepository: TodoRepository;
   
   constructor() {
     this.registerDependencies();
     
     // Resolve the TodoListRenderer from the container
     this.todoListRenderer = container.resolve<TodoListRenderer>('TodoListRenderer');
+    this.todoRepository = container.resolve<TodoRepository>('TodoRepository');
     this.initialize();
   }
 
@@ -20,7 +23,8 @@ class Main {
    */
   private initialize(){
     console.log('Main class initialization logic');
-    this.todoListRenderer.renderTodoList();
+    this.registerHandlers();
+    this.todoListRenderer.renderTodoList(this.todoRepository.getAllTodos());
   }
 
   /**
@@ -30,8 +34,29 @@ class Main {
    */
   private registerDependencies() {
     container.register('TodoRepository', { useClass: TodoRepository });
-    container.register('TodoListRenderer', {
-      useFactory: (c) => new TodoListRenderer(c.resolve('TodoRepository')),
+    container.register('TodoListRenderer', { useClass: TodoListRenderer });
+  }
+  
+  /**
+   * Registers event handlers for the application - e.g. form submission or events for buttons
+   * @private
+   */
+  private registerHandlers() {
+    document.getElementById('todo-form')?.addEventListener('submit', (e: Event) => {
+      e.preventDefault();
+      const titleInput = (document.getElementById('new-todo-input') as HTMLInputElement).value.trim();
+      
+      // Create a new todo item
+      const todo: Todo = {
+        id: null,
+        title: titleInput,
+        updatedAt: new Date(),
+        createdAt: new Date(),
+        completed: false
+      };
+      
+      this.todoRepository.upsertTodo(todo);
+      this.todoListRenderer.renderTodoList(this.todoRepository.getAllTodos());
     });
   }
 }
