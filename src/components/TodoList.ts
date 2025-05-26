00b5c55ccc11465835979442b3a96203c0096308
Michaela Hošková <66723714+misahoskova@users.noreply.github.com>
2025-05-26 14:07:32 +0200

    Loader

--- src/components/TodoList.ts
+++ src/components/TodoList.ts
@@ -6,44 +6,77 @@ class TodoList {
 
   todoListElement
 
+  isLoading: boolean = false
+
   constructor(elementId: string = 'todo-list') {
     this.todoListElement = document.getElementById(elementId)! as HTMLUListElement
     this.loadTodos()
   }
 
   async loadTodos() {
-    const newTodos = await todoService.fetchTodos()
-    this.todos = newTodos
-    this.render()
+    try {
+      this.isLoading = true
+      this.render()
+      const newTodos = await todoService.fetchTodos()
+      this.isLoading = false
+      this.todos = newTodos
+      this.render()
+    } catch (error) {
+      console.error('Error loading todos:', error)
+      this.todoListElement.innerHTML = '<li>Error loading todos. Please try again later.</li>'
+    } finally {
+      this.isLoading = false
+      this.render()
+    }
   }
 
   async addTodo(todoValue: string) {
-    const newTodo = await todoService.createTodo(todoValue)
-    this.todos.push(newTodo)
-    this.render()
+    try {
+      const newTodo = await todoService.createTodo(todoValue)
+      this.todos.push(newTodo)
+      this.render()
+    } catch (error) {
+      console.error('Error adding todo:', error)
+      this.todoListElement.innerHTML = '<li>Error adding todo. Please try again later.</li>'
+    }
   }
 
   async removeTodo(id: number) {
-    await todoService.deleteTodo(id)
-    this.todos = this.todos.filter((todo) => todo.id !== id)
-    this.render()
+    try {
+      await todoService.deleteTodo(id)
+      this.todos = this.todos.filter((todo) => todo.id !== id)
+      this.render()
+    } catch (error) {
+      console.error('Error removing todo:', error)
+      this.todoListElement.innerHTML = '<li>Error removing todo. Please try again later.</li>'
+    }
   }
 
   async toggle(id: number) {
     const todo = this.todos.find((todo) => todo.id === id)
-    const newTodo = await todoService.toggleTodo(id, !todo?.completed)
+    try {
+      const newTodo = await todoService.toggleTodo(id, !todo?.completed)
 
-    this.todos = this.todos.map((todo) => {
-      if (todo.id === id) {
-        return newTodo
-      }
-      return todo
-    })
-    this.render()
+      this.todos = this.todos.map((todo) => {
+        if (todo.id === id) {
+          return newTodo
+        }
+        return todo
+      })
+      this.render()
+    } catch (error) {
+      console.error('Error toggling todo:', error)
+      this.todoListElement.innerHTML = '<li>Error toggling todo. Please try again later.</li>'
+    }
   }
 
   render() {
     this.todoListElement.innerHTML = ''
+    if (this.isLoading) {
+      this.todoListElement.appendChild(createLoadingSpinner())
+      return
+    }
+
     this.todos.forEach((item) => {
       const todoItemElement = document.createElement('li')
       const todoSpanElement = document.createElement('span')
@@ -70,6 +103,16 @@ class TodoList {
       this.todoListElement.appendChild(todoItemElement)
     })
   }
+}
+
+function createLoadingSpinner() {
+  const container = document.createElement('div')
+  const spinner = document.createElement('div')
+  container.className = 'loading-container'
+  spinner.className = 'loading-spinner'
+
+  container.appendChild(spinner)
+  return container
 }
 
 export const todoList = new TodoList('todo-list')
