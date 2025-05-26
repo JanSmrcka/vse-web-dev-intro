859764b1b1214d0370f588b2a8d345a69461aff1
Trong Dat Luu <david19au@gmail.com>
2025-05-26 14:36:59 +0200

    pulsing while loading

--- src/components/todo-list.ts
+++ src/components/todo-list.ts
@@ -5,13 +5,14 @@ class TodoList {
   todos: Todo[] = []
   todoListElement = document.getElementById('todo-list') as HTMLUListElement
   isLoading: boolean = false
+  isPulsing: boolean = false
 
   constructor(elementId: string) {
     this.todoListElement = document.getElementById(elementId) as HTMLUListElement
     this.loadTodos()
   }
 
-  async loadTodos() {
+  private async loadTodos() {
     try {
       this.isLoading = true
       this.render()
@@ -29,42 +30,72 @@ class TodoList {
 
   async addTodo(todoValue: string) {
     try {
+      this.isLoading = true
+      this.render()
       const newTodo = await todoService.createTodo(todoValue)
       this.todos.push(newTodo)
+      this.isLoading = false
       this.render()
     } catch (error) {
       console.error(error)
+    } finally {
+      this.isLoading = false
+      this.render()
     }
   }
 
   async removeTodo(id: number) {
     try {
+      this.isLoading = true
+      this.render()
       await todoService.deleteTodo(id)
       this.todos = this.todos.filter((todo) => todo.id !== id)
+      this.isLoading = false
       this.render()
-    } catch (error) {}
+    } catch (error) {
+      console.error(error)
+    } finally {
+      this.isLoading = false
+      this.render()
+    }
   }
 
   async toggle(id: number) {
-    const todo = this.todos.find((todo) => id === todo.id)
-    const newTodo = await todoService.toggleTodo(id, !todo?.completed)
-
-    this.todos = this.todos.map((todo) => {
-      if (todo.id === id) {
-        return newTodo
-      }
-      return todo
-    })
-    console.log(this.todos)
-    this.render()
+    try {
+      this.isLoading = true
+      this.render()
+      const todo = this.todos.find((todo) => id === todo.id)
+      const newTodo = await todoService.toggleTodo(id, !todo?.completed)
+      this.isLoading = false
+      this.todos = this.todos.map((todo) => {
+        if (todo.id === id) {
+          return newTodo
+        }
+        return todo
+      })
+      console.log(this.todos)
+      this.render()
+    } catch (error) {
+      console.error(error)
+    } finally {
+      this.isLoading = false
+      this.render()
+    }
   }
 
-  render() {
+  private render() {
     this.todoListElement.innerHTML = ''
 
-    if (this.isLoading) {
+    if (this.isLoading === true && this.todos.length === 0) {
       this.todoListElement.appendChild(createLoadingSpinner())
       return
+    }
+
+    if (this.isLoading) {
+      this.todoListElement.classList.add('isLoading')
+    }
+    if (!this.isLoading) {
+      this.todoListElement.classList.remove('isLoading')
     }
 
     this.todos.forEach((item) => {
