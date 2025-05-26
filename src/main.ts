cafb0298dc9567c433e89c3491597b769e5a20cf
Adam <adam.schindler@seznam.cz>
2025-05-26 10:58:34 +0200

    sktrani hotovych polozek, mazani

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,28 @@
 const formElement = document.getElementById('todo-form')! as HTMLFormElement
 
-const todos: string[] = []
+type Todo = {
+    id: string
+    text: string
+    completed: boolean
+}
+
+let todos: Todo[] = []
 
 function handleFormSubmit(e: Event) {
   e.preventDefault()
   const formData = new FormData(formElement)
+
   const todoValue = formData.get('todo-text') as string
-  todos.push(todoValue)
+
+  const newTodo: Todo = {
+    id:crypto.randomUUID(),
+    text: todoValue,
+    completed: false,
+  }
+
+  todos.push(newTodo)
   renderTodos()
+  formElement.reset()
   console.log(todos)
 }
 
@@ -17,16 +32,37 @@ function renderTodos() {
   const todoListElement = document.getElementById('todo-list') as HTMLUListElement
   todoListElement.innerHTML = ''
 
-  todos.forEach((value) => {
+  todos.forEach((item) => {
     const todoItemElement = document.createElement('li')
     const todoSpanElement = document.createElement('span')
-    todoSpanElement.innerHTML = value
+    todoSpanElement.innerHTML = item.text
+
+    if(item.completed){
+        todoItemElement.classList.add("completed")
+    }
+
+    todoItemElement.addEventListener("click", ()=>{
+        todos = todos.map((todo)=>{
+            if(todo.id === item.id){
+                return {...todo, completed: !todo.completed }
+            }
+            return todo
+        })
+        console.log(todos)
+        renderTodos()
+    })
+
     const deleteButton = document.createElement('button')
     deleteButton.innerHTML = 'delete'
 
+    deleteButton.addEventListener("click",()=>{
+        todos = todos.filter((todo)=>todo.id !== item.id)
+        console.log(todos)
+        renderTodos()
+    })
+
     todoItemElement.appendChild(todoSpanElement)
     todoItemElement.appendChild(deleteButton)
-
     todoListElement?.appendChild(todoItemElement)
   })
 }
