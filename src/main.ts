2687f79b5e8059cbd5aff99a5eb07e0efc035ead
Trong Dat Luu <david19au@gmail.com>
2025-05-26 10:04:28 +0200

    working add todos

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,25 @@
 const formElement = document.getElementById('todo-form')! as HTMLFormElement;
 
-
+const todos: string[] = []
 
 function handleFormSubmit(e: Event){
     e.preventDefault()
     const formData = new FormData(formElement)
     const todoValue = formData.get("todo-text") as string
-    console.log(todoValue)
+    todos.push(todoValue)
+    renderTodos()
+    console.log(todos)
 }
 
 formElement.addEventListener('submit',handleFormSubmit)
 
+function renderTodos() {
+    const todoListElement = document.getElementById('todo-list') as HTMLUListElement
+    todoListElement.innerHTML = ''
+    todos.forEach((value, index) => {
+        const todoItemElement = document.createElement('li')
+        todoItemElement.innerHTML = value
+        todoListElement?.appendChild(todoItemElement)
+
+    })
+}
