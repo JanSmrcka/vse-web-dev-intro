e614842bb8604b4deb82b4586bf7e2ad2f02134b
Jakub Kafka <info@goliathus.net>
2025-05-26 11:23:00 +0200

    refactor todo management by creating a TodoList class; move todo-related logic to components and define Todo type in a separate file

---
+++ src/components/todo-list.ts
@@ -0,0 +1,67 @@
+import { Todo } from "../types";
+
+class TodoList {
+    todos: Todo[] = [];
+    todoListElement;
+
+    constructor(elementId: string) {
+        this.todoListElement = document.getElementById(elementId) as HTMLUListElement;;
+    }
+
+    addTodo(todoValue: string) {
+        const newTodo: Todo = {
+            id: crypto.randomUUID(),
+            text: todoValue,
+            completed: false,
+        }
+
+        this.todos.push(newTodo);
+        this.render();
+    };
+
+    removeTodo(id: string) {
+        this.todos = this.todos.filter((todo) => todo.id !== id);
+        this.render();
+    };
+
+    toggleTodo(id: string) {
+        this.todos = this.todos.map((todo) => {
+            if (todo.id === id) {
+                return { ...todo, completed: !todo.completed };
+            }
+            return todo;
+        });
+        this.render();
+    };
+
+    render() {
+        this.todoListElement.innerHTML = '';
+        this.todos.forEach((item) => {
+            const todoItemElement = document.createElement('li');
+            const todoSpanElement = document.createElement('span');
+            todoSpanElement.innerHTML = item.text;
+    
+            if (item.completed) {
+                todoItemElement.classList.add('completed');
+            }
+    
+            todoSpanElement.addEventListener('click', () => {
+                this.toggleTodo(item.id);
+            });
+            
+            const deleteButtonElement = document.createElement('button');
+            deleteButtonElement.innerHTML = 'Delete';
+    
+            deleteButtonElement.addEventListener('click', () => {
+                this.removeTodo(item.id);
+            });
+    
+            todoItemElement.appendChild(todoSpanElement);
+            todoItemElement.appendChild(deleteButtonElement);
+            
+            this.todoListElement?.appendChild(todoItemElement);
+        });
+    };
+};
+
+export const todoList = new TodoList('todo-list');
--- src/main.ts
+++ src/main.ts
@@ -1,66 +1,15 @@
+import { todoList } from "./components/todo-list";
+
 const formElement = document.getElementById('todo-form')! as HTMLFormElement;
-
-type Todo = {
-    id: string;
-    text: string;
-    completed: boolean;
-}
-
-let todos: Todo[] = [];
 
 function handleFormSubmit(e: Event) {
     e.preventDefault();
     const formData = new FormData(formElement);
     const todoValue = formData.get('todo-text') as string;
 
-    const newTodo: Todo = {
-        id: crypto.randomUUID(),
-        text: todoValue,
-        completed: false,
-    }
+    todoList.addTodo(todoValue);
 
-    todos.push(newTodo);
-    renderTodos();
     formElement.reset();
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
-
-function renderTodos() {
-    const todoListElement = document.getElementById('todo-list') as HTMLUListElement;
-    todoListElement.innerHTML = '';
-
-    todos.forEach((item) => {
-        const todoItemElement = document.createElement('li');
-        const todoSpanElement = document.createElement('span');
-        todoSpanElement.innerHTML = item.text;
-
-        if (item.completed) {
-            todoItemElement.classList.add('completed');
-        }
-
-        todoSpanElement.addEventListener('click', () => {
-            todos = todos.map((todo) => {
-                if (todo.id === item.id) {
-                    return { ...todo, completed: !todo.completed };
-                }
-                return todo;
-            });
-            console.log(todos);
-            renderTodos();
-        });
-        
-        const deleteButtonElement = document.createElement('button');
-        deleteButtonElement.innerHTML = 'Delete';
-
-        deleteButtonElement.addEventListener('click', () => {
-            todos = todos.filter((todo) => todo.id !== item.id);
-            renderTodos();
-        });
-
-        todoItemElement.appendChild(todoSpanElement);
-        todoItemElement.appendChild(deleteButtonElement);
-        
-        todoListElement?.appendChild(todoItemElement);
-    });
-}
