efb7373372c443a3d20fc28c5c6d8d207f1794fa
Petr Stipek <stipepet@fit.cvut.cz>
2025-05-26 13:06:05 +0200

    createTodo

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -2,10 +2,17 @@ import { Todo } from '../types';
 
 const API_URL = "https://eli-workshop.vercel.app/api/users/stip05/todos";
 
+class ApiError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = "Api Error";
+    }
+};
+
 // handling the response from the API, showin an error if the response is not ok
 const handleResponse = async <T>(response: Response): Promise<T> => {
     if (!response.ok) {
-        throw new Error(`API failed!: ${response.status}`);
+        throw new ApiError(`API request failed: ${response.status} - ${response.statusText}`);
     };
     const data = await response.json();
     return data;
@@ -13,8 +20,23 @@ const handleResponse = async <T>(response: Response): Promise<T> => {
 
 // fetching todos from the API
 export const todoService = {
+    // Fetching todos from the API
     async fetchTodos() {
         const response = await fetch(API_URL);
         return handleResponse<Todo[]>(response);
-    }
+    },
+    // Creating a new todo item
+    async createTodo(newTodo: string) {
+        const body = {
+            text: newTodo
+        };
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body),
+        });
+        return handleResponse<Todo>(response);
+    },
 };
