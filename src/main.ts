0acce02be80722907d18411a57fb3d4dec296c24
adamhanus <kecupwik@gmail.com>
2025-05-26 10:56:29 +0200

    refactor todo handling to use structured Todo objects; implement toggle and delete functionality

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,27 @@
-const formElement = document.getElementById('todo-form')
+const formElement = document.getElementById('todo-form') as HTMLFormElement
 
-const todos: string[] = []
+type Todo = {
+    id: string
+    title: string
+    completed: boolean
+}
+
+let todos: Todo[] = []
 
 function handleFormSubmit(e: Event) {
   e.preventDefault()
   const formData = new FormData(formElement)
   const todoValue = formData.get('todo-text') as string
-  todos.push(todoValue)
+
+  const newTodo: Todo = {
+    id: crypto.randomUUID(),
+    title: todoValue,
+    completed: false
+  }
+
+  todos.push(newTodo)
   renderTodos()
+  formElement.reset()
 }
 
 formElement?.addEventListener("submit", handleFormSubmit) // Otaznik - pokud je element NULL, tak to projde a nespadne
@@ -16,12 +30,39 @@ function renderTodos() {
   const todoListElement = document.getElementById('todo-list') as HTMLUListElement
   todoListElement.innerHTML = ''
 
-  todos.forEach((value) => {
+  todos.forEach((item) => {
     const todoItemElement = document.createElement('li')
     const todoSpanElement = document.createElement('span')
-    todoSpanElement.innerHTML = value
+    todoSpanElement.innerHTML = item.title
+
+    if (item.completed) {
+        todoItemElement.classList.add("completed")
+    }
+
+    todoItemElement.addEventListener("click", () => {
+        todos = todos.map((todo) => {
+            if (todo.id === item.id) {
+                return {...todo, completed: !todo.completed} //spread operator - kopie objektu a zmenin v tomto pripade completed
+            }
+            return todo
+        })
+        console.log(todos)
+        renderTodos()
+    })
+
+
     const deleteButton = document.createElement('button')
     deleteButton.innerHTML = "Delete"
+
+    deleteButton.addEventListener("click", (e) => {
+        e.stopPropagation()
+        todos = todos.filter((todo ) => todo.id !== item.id )
+        renderTodos()
+    })
+
+
+
+
 
     todoItemElement.appendChild(todoSpanElement)
     todoItemElement.appendChild(deleteButton)
