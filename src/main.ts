2a81710774dda107ca24f6155ee03fbf73f02c09
Petr Stipek <stipepet@fit.cvut.cz>
2025-05-26 10:51:47 +0200

    deleting todos and changing styles when completed

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 const formElement = document.getElementById("todo-form") as HTMLFormElement;
+type Todo = {
+    id: string;
+    text: string;
+    completed: boolean;
+};
 
-const todos: Array<string> = [];
+let todos: Array<Todo> = [];
 
 function handleFormSubmit(e: Event) {
     e.preventDefault();
     const formData = new FormData(formElement);
     const todoValue = formData.get("todo-text") as string;
-    //console.log(todoValue);
-    todos.push(todoValue);
-    //console.log(todos);
+
+    const newTodo: Todo = {
+        id: crypto.randomUUID(),
+        text: todoValue,
+        completed: false
+    };
+
+    todos.push(newTodo);
     renderTodos();
     formElement.reset();
 };
@@ -18,13 +28,38 @@ formElement.addEventListener("submit", handleFormSubmit);
 function renderTodos() {
     const todoListElement = document.getElementById("todo-list") as HTMLUListElement;
     todoListElement.innerHTML = "";
-    todos.forEach((value) => {
+    todos.forEach((item) => {
         const todoItemElement = document.createElement("li");
         const todoSpanElement = document.createElement("span");
         const deleteButton = document.createElement("button");
 
-        todoSpanElement.innerHTML = value;
+        //set completed class
+        if (item.completed) {
+            todoItemElement.classList.add("completed");
+        };
+
+        //item element event listener
+        todoItemElement.addEventListener("click", () => {
+            todos = todos.map((todo) => {
+                if (todo.id === item.id) {
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    }
+                };
+                return todo;
+            });
+            renderTodos();
+        });
+
+        todoSpanElement.innerHTML = item.text;
         deleteButton.innerHTML = "Delete";
+
+        //delete button event listener
+        deleteButton.addEventListener("click", () => {
+            todos = todos.filter((todo) => todo.id !== item.id);
+            renderTodos();
+        });
 
         todoItemElement.appendChild(todoSpanElement);
         todoItemElement.appendChild(deleteButton);
